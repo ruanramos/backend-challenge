f296e00c6b3445c4f35fed9fe3f474060c739c51
Ruan da Fonseca Ramos <ruan.ramos28@gmail.com>
2024-08-29 23:30:56 -0300

    Add initial impl

---
+++ src/main/java/health/tabia/challenge/MetricIteratorImpl.java
@@ -0,0 +1,25 @@
+﻿package health.tabia.challenge;
+
+public class MetricIteratorImpl implements MetricIterator {
+
+
+    @Override
+    public boolean moveNext() {
+        return false;
+    }
+
+    @Override
+    public Metric current() {
+        return null;
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+}
---
+++ src/main/java/health/tabia/challenge/MetricStoreImpl.java
@@ -0,0 +1,16 @@
+package health.tabia.challenge;
+
+public class MetricStoreImpl implements MetricStore{
+
+    public void insert(Metric metric) {
+
+    }
+
+    public void removeAll(String name) {
+
+    }
+
+    public MetricIterator query(String name, long from, long to) {
+        return null;
+    }
+}
