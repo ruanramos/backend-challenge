7b85190587c8d3655de32ca05b080fe203db17b9
Ruan da Fonseca Ramos <ruan.ramos28@gmail.com>
2024-08-29 23:33:20 -0300

    Add test files

---
+++ src/test/health/tabia/challenge/MetricIteratorImplTest.java
@@ -0,0 +1,24 @@
+﻿package health.tabia.challenge;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MetricIteratorImplTest {
+
+    @Test
+    void moveNext() {
+    }
+
+    @Test
+    void current() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void close() {
+    }
+}
---
+++ src/test/health/tabia/challenge/MetricStoreImplTest.java
@@ -0,0 +1,18 @@
+﻿package health.tabia.challenge;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MetricStoreImplTest {
+
+    @org.junit.jupiter.api.Test
+    void insert() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeAll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void query() {
+    }
+}
